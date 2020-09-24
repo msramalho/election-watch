62c9d9c193b7b59e1b87c0408b4d7ed8fb9fe9ca
msramalho <up201403027@fe.up.pt>
2020-09-25 01:15:25 +0200

    new task_data/?task_name endpoint created

--- core/utils/task.py
+++ core/utils/task.py
@@ -6,10 +6,17 @@ from .misc import get_filter_by_day
 class Task:
     # handles a collection task_name
     # each document is {_id: uuid, day: day of analysis, data: what to store for this day}
-    def __init__(self, db, name):
-        self.name = slugify(name, separator="_")
-        self.collection = db["task_%s" % self.name]
+    def __init__(self, db, name, create=True):
+        self.name = "task_%s" % slugify(name, separator="_")
+        self.db = db
+        if create or self.exists(): self.create()
+
+    def create(self):
+        self.collection = self.db[self.name]
         # self.collection.create_index("day") # create index on day
+
+    def exists(self):
+        return self.name in self.db.list_collection_names()
 
     def exists_day(self, day):
         return self.collection.find_one({"day": get_filter_by_day(day)}) is not None
@@ -17,11 +24,11 @@ class Task:
     def insert(self, day, data):
         return self.collection.insert({"_id": str(uuid.uuid4()), "day": day, "data": data})
 
-    def get_last_n(self, n=30):
+    def get_last_n(self, n=30, withId=True):
         assert n > 0, "n must be greater than 0"
         # retrieve last n entries concatenated
         # self.collection.find({})
-        return list(self.collection.find({}).sort([("day", -1)]).limit(n))
+        return list(self.collection.find({}, {"_id": withId, "day": 1, "data": 1}).sort([("day", -1)]).limit(n))
 
     def unzip_last_n(self, n=30):
         # returns two lists [days], [datas]
@@ -31,3 +38,8 @@ class Task:
     def get_last(self): return self.get_last_n(1)[0]
 
     def drop(self): self.collection.remove({})
+
+    def get_api_n(self, n):
+        # returns a standardized dict that can be returned directly by the api
+        res = map(lambda r: {"day": r["day"].strftime("%Y-%m-%d"), "data": r["data"]}, self.get_last_n(n, False))
+        return {"data": list(res)}
