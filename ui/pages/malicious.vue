9d426a67ca04a19964e309fc78d581f3867074b9
msramalho <up201403027@fe.up.pt>
2020-09-27 01:03:52 +0200

    wtv

--- ui/pages/malicious.vue
+++ ui/pages/malicious.vue
@@ -2,6 +2,14 @@
   <v-layout>
     <v-flex class="text-center">
       <h1>Atividade Maliciosa</h1>
+      <fakenews />
     </v-flex>
   </v-layout>
 </template>
+
+<script>
+import Fakenews from "~/components/general/Fakenews.vue";
+export default {
+  components: { Fakenews },
+};
+</script>
