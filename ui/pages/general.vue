c97c56bf5e63a98c19bcf76a72e81189e00631b6
msramalho <up201403027@fe.up.pt>
2020-09-27 23:21:15 +0200

    bamus

--- ui/pages/general.vue
+++ ui/pages/general.vue
@@ -2,14 +2,20 @@
   <v-layout>
     <v-flex class="text-center">
       <h1>Visão Geral</h1>
+      <p class="ma-5">
+        Nesta página encontra-se uma descrição geral das contas e respetivos
+        tweets que estamos a monitorizar, no contexto do Twitter Português.
+      </p>
       <tweet-types />
+      <creation-dates />
     </v-flex>
   </v-layout>
 </template>
 
 <script>
 import TweetTypes from "~/components/general/TweetTypes.vue";
+import CreationDates from "~/components/general/CreationDates.vue";
 export default {
-  components: { TweetTypes },
+  components: { TweetTypes, CreationDates },
 };
 </script>
